1b3e6f1aadc9cbc40956a8dbb7e75c09c4fed1af
Seth <seth@Zipcoders-MacBook-Pro.local>
2023-04-08 10:49:48 -0400

    passed all tests, love me a 5 minute lab

--- src/main/zipcode/Hamming.java
+++ src/main/zipcode/Hamming.java
@@ -1,10 +1,21 @@
 package zipcode;
 
 public class Hamming {
-    public Hamming(String s, String s1) {
+    String left;
+    String right;
+    public Hamming(String left, String right) {
+        if(left.length()!= right.length()){
+            throw new IllegalArgumentException("leftStrand and rightStrand must be of equal length.");
+        }
+        this.left = left;
+        this.right = right;
     }
 
     public int getHammingDistance() {
-        return -1;
+        int count = 0;
+        for (int i = 0; i < left.length(); i++) {
+            count = (left.charAt(i) != right.charAt(i)) ? count + 1: count;
+        }
+        return count;
     }
 }
